d25d7011d8380a13e25bbbac8d3aed2da3768130
daniel <lendoon@gmail.com>
2017-02-12 15:49:38 +0800

    add and config spring

---
+++ vehicles_service/src/main/java/com/cooltee/service/impl/UserServiceImpl.java
@@ -0,0 +1,10 @@
+package com.cooltee.service.impl;
+
+import com.cooltee.service.interfaces.UserService;
+
+/**
+ * implements Interface of UserSersive
+ * Created by Daniel on 2017/2/11.
+ */
+public class UserServiceImpl implements UserService {
+}
---
+++ vehicles_service/src/main/java/com/cooltee/service/interfaces/UserService.java
@@ -0,0 +1,8 @@
+package com.cooltee.service.interfaces;
+
+/**
+ * The Interface of UserService
+ * Created by Daniel on 2017/2/11.
+ */
+public interface UserService {
+}
