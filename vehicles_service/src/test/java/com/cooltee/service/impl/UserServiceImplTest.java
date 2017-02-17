1c2290241ad83630bdee5d5c6d030a1403595560
daniel <lendoon@gmail.com>
2017-02-17 19:14:48 +0800

    add test ability to service to test

---
+++ vehicles_service/src/test/java/com/cooltee/service/impl/UserServiceImplTest.java
@@ -0,0 +1,11 @@
+package com.cooltee.service.impl;
+
+import static org.junit.Assert.*;
+
+/**
+ * TODO add distributions about this class
+ * Created by Daniel on 2017/2/17.
+ */
+public class UserServiceImplTest {
+
+}
