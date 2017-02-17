429a167d4389ce139b1fd70d42e12dd2c9abf2b4
daniel <lendoon@gmail.com>
2017-02-17 19:14:12 +0800

    add test ability to service to test

---
+++ vehicles_service/src/test/java/TransSpringTestSupport.java
@@ -0,0 +1,11 @@
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
+
+/**
+ * Base abstract class of text, use to upload spring context;
+ * Note: No Transactional Test should extends this.
+ * Created by Daniel on 2017/2/14.
+ */
+@ContextConfiguration(locations = {"classpath:/spring/spring-*.xml"})
+public abstract class TransSpringTestSupport extends AbstractTransactionalJUnit4SpringContextTests {
+}
