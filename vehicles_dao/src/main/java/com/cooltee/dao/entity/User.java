6e0b12d08b4b1d83cc720809cf763f5bd1a4fd07
alittleseven <a7752177>
2017-02-19 21:19:12 +0800

    by Steven

--- vehicles_dao/src/main/java/com/cooltee/dao/entity/User.java
+++ vehicles_dao/src/main/java/com/cooltee/dao/entity/User.java
@@ -4,11 +4,13 @@ import com.cooltee.dao.orm.BaseEntity;
 import org.hibernate.annotations.Cache;
 
 import javax.persistence.*;
+import java.util.Date;
 
 /**
  * TODO add distributions about this class
  * Created by Daniel on 2017/2/12.
  */
+
 @Entity
 @Table(name = "veh_user")
 public class User extends BaseEntity {
@@ -21,22 +23,42 @@ public class User extends BaseEntity {
     @Column(name = "name")
     private String name;
 
-    @Override
-    public void setId(Long id) {
-        this.id = id;
-    }
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "state")
+    private int state;
+
+    @Column(name = "create_time")
+    private Date create_time;
+
+    @Column(name = "update_time")
+    private Date update_time;
+
 
     @Override
-    public Long getId() {
-        return id;
-    }
+    public void setId(Long id) { this.id = id; }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+    @Override
+    public Long getId() { return id; }
 
-    public String getName() {
-        return name;
-    }
+    public void setName(String name) { this.name = name; }
 
+    public String getName() { return name; }
+
+    public void setPassword(String password) { this.password = password; }
+
+    public String getPassword() { return password; }
+
+    public void setState(int state) { this.state = state; }
+
+    public int getState() { return state; }
+
+    public void setCreate_time(Date create_time) { this.create_time = create_time; }
+
+    public Date getCreate_time() { return create_time; }
+
+    public void setUpdate_time(Date update_time) { this.update_time = update_time; }
+
+    public Date getUpdate_time() { return update_time; }
 }
