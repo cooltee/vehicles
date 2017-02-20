f19ce78e492e8063cc2ae7b485563d4bfb3caa1f
daniel <lendoon@gmail.com>
2017-02-20 10:58:12 +0800

    mysql manager timestamp

--- vehicles_dao/src/main/resources/sql/vehicles.mysql5.create.sql
+++ vehicles_dao/src/main/resources/sql/vehicles.mysql5.create.sql
@@ -5,8 +5,8 @@ create table veh_user
    password             varchar(16) comment '密码',
    comments             varchar(64) comment '备注',
    state                tinyint(1) comment '状态',
-   create_time          timestamp comment '创建时间',
-   update_time          timestamp comment '更新时间',
+   create_time          timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP comment '创建时间',
+   update_time          timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP comment '更新时间',
    primary key (id)
 );
 
@@ -18,8 +18,8 @@ create table veh_group
    name                 varchar(16) comment '姓名',
    comments             varchar(64) comment '备注',
    state                tinyint(1) comment '状态',
-   create_time          timestamp comment '创建时间',
-   update_time          timestamp comment '更新时间',
+   create_time          timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP comment '创建时间',
+   update_time          timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP comment '更新时间',
    primary key (id)
 );
 
@@ -30,8 +30,8 @@ create table veh_permission
    id                   bigint(16) not null comment '主键id',
    name                 varchar(16),
    permission_code      varchar(16),
-   create_time          timestamp comment '创建时间',
-   update_time          timestamp comment '更新时间',
+   create_time          timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP comment '创建时间',
+   update_time          timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP comment '更新时间',
    primary key (id)
 );
 
@@ -47,8 +47,8 @@ create table veh_vehicle
    purchase_dept        varchar(32) comment '购入部门',
    purchase_date        date comment '购入日期',
    archives_no          varchar(32) comment '档案编号',
-   create_time          timestamp comment '创建时间',
-   update_time          timestamp comment '更新时间',
+   create_time          timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP comment '创建时间',
+   update_time          timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP comment '更新时间',
    primary key (id)
 );
 
@@ -64,8 +64,8 @@ create table veh_driver
    dept                 varchar(16) comment '所属部门',
    allow_date           date comment '准驾日期',
    allow_type           tinyint(4) comment '准驾车型',
-   create_time          timestamp comment '创建时间',
-   update_time          timestamp comment '更新时间',
+   create_time          timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP comment '创建时间',
+   update_time          timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP comment '更新时间',
    primary key (id)
 );
 
@@ -77,8 +77,8 @@ create table veh_drive_record
    driver_id            bigint(16) comment '驾驶员id',
    vehicle_id           bigint(16) comment '车辆id',
    distance             double(16,3) comment '总路程（km）',
-   create_time          timestamp comment '创建时间',
-   update_time          timestamp comment '更新时间',
+   create_time          timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP comment '创建时间',
+   update_time          timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP comment '更新时间',
    primary key (id)
 );
 
@@ -97,8 +97,8 @@ create table veh_cost
    amount               decimal(16,2) not null comment '金额（￥）',
    cost_type            tinyint(1) comment '花费类型',
    spend_date           date comment '花费日期',
-   create_time          timestamp comment '创建时间',
-   update_time          timestamp comment '更新时间',
+   create_time          timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP comment '创建时间',
+   update_time          timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP comment '更新时间',
    primary key (id)
 );
 
@@ -112,8 +112,8 @@ create table veh_other_record
    id                   bigint(16) not null comment '主键id',
    cost_id              bigint(16) comment '费用id',
    description          varchar(255) comment '费用说明',
-   create_time          timestamp,
-   update_time          timestamp,
+   create_time          timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP comment '创建时间',
+   update_time          timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP comment '更新时间',
    primary key (id)
 );
 
@@ -124,13 +124,13 @@ alter table veh_other_record add constraint FK_Reference_6 foreign key (cost_id)
 
 create table veh_oil_record
 (
-   id                   bigint(16) not null,
+   id                   bigint(16) not null comment '主键id',
    cost_id              bigint(16) not null comment '主键id',
    oil_price            decimal(16,2) comment '燃油单价（￥/L）',
    oil_volume           decimal(16,3) comment '燃油体积（L）',
    oil_discount         decimal(3,2) comment '燃油折扣',
-   create_time          timestamp comment '创建时间',
-   update_time          timestamp comment '更新时间',
+   create_time          timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP comment '创建时间',
+   update_time          timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP comment '更新时间',
    primary key (id)
 );
 
@@ -141,11 +141,11 @@ alter table veh_oil_record add constraint FK_Reference_3 foreign key (cost_id)
 
 create table veh_repair_record
 (
-   id                   bigint(16) not null,
+   id                   bigint(16) not null comment '主键id',
    cost_id              bigint(16) comment '费用id',
    description          varchar(255) comment '维修说明',
-   create_time          timestamp,
-   update_time          timestamp,
+   create_time          timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP comment '创建时间',
+   update_time          timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP comment '更新时间',
    primary key (id)
 );
 
