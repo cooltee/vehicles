e68e625a647799402b71a06e5b51ad47f383cc81
daniel <lendoon@gmail.com>
2017-02-20 20:32:45 +0800

    repository to spring

--- vehicles_dao/src/main/java/com/cooltee/dao/impl/UserGroupRelaDaoImpl.java
+++ vehicles_dao/src/main/java/com/cooltee/dao/impl/UserGroupRelaDaoImpl.java
@@ -3,10 +3,12 @@ package com.cooltee.dao.impl;
 import com.cooltee.dao.entity.UserGroupRela;
 import com.cooltee.dao.interfaces.UserGroupRelaDao;
 import com.cooltee.dao.orm.BaseDaoImpl;
+import org.springframework.stereotype.Repository;
 
 /**
  * dao of UserGroupRela
  * Created by alittleseven on 2017/2/20.
  */
+@Repository
 public class UserGroupRelaDaoImpl extends BaseDaoImpl<UserGroupRela> implements UserGroupRelaDao {
 }
