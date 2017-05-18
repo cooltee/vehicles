d9aab4c18f5c33f3511803f2745c82a3c845e655
daniel <lendoon@gmail.com>
2017-05-18 11:12:35 +0800

    add String util class: generate UUID

---
+++ vehicles_util/src/main/java/com/cooltee/util/StringUtil.java
@@ -0,0 +1,14 @@
+package com.cooltee.util;
+
+import java.util.UUID;
+
+/**
+ * Utilize class of String
+ * Created by Daniel on 2017/5/18.
+ */
+public class StringUtil {
+
+	public static String generateUUID() {
+		return UUID.randomUUID().toString();
+	}
+}
