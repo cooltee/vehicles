85538c0fbfdd278e5304f1fd9554423af678444a
daniel <lendoon@gmail.com>
2017-06-03 01:13:33 +0800

    add identify service

---
+++ vehicles_service/src/main/java/com/cooltee/service/impl/IdentifyServiceImpl.kt
@@ -0,0 +1,12 @@
+package com.cooltee.service.impl
+
+import com.cooltee.service.interfaces.IdentifyService
+import org.springframework.stereotype.Service
+
+/**
+ * Implement Identify Interface
+ * Created by Daniel on 2017/5/31.
+ */
+@Service
+class IdentifyServiceImpl : IdentifyService {
+}
---
+++ vehicles_service/src/main/java/com/cooltee/service/interfaces/IdentifyService.kt
@@ -0,0 +1,8 @@
+package com.cooltee.service.interfaces
+
+/**
+ * The Interface of Identify signed user authority by group
+ * Created by Daniel on 2017/5/31.
+ */
+interface IdentifyService {
+}
