a93832d228162c0ed4078710f40cf8374d064d2d
daniel <lendoon@gmail.com>
2017-06-02 10:19:54 +0800

    add change password js

--- vehicles_web/src/main/webapp/statics/js/script.js
+++ vehicles_web/src/main/webapp/statics/js/script.js
@@ -24,4 +24,48 @@ function loadOnFrame(target) {
 
 $(function () {
     loadOnFrame(contentPath + "/page/welcome");
+
+    $('.chgPass-form input[type="text"], .chgPass-form input[type="password"], .chgPass-form textarea').on('focus', function() {
+        $(this).removeClass('input-error');
+    });
+
+    //Form validation and submit
+    $('.chgPass-form').on('submit', function(e) {
+
+        var flag = true;
+        $(this).find('input[type="password"]').each(function(){
+            if( $(this).val() === "" ) {
+                e.preventDefault();
+                $(this).addClass('input-error');
+                var msg = $('.msg');
+                msg.addClass("msg-error");
+                msg[0].innerText = "两次密码输入不相同，请重新输入！";
+                flag = false;
+            }
+            else {
+                $(this).removeClass('input-error');
+            }
+        });
+        /*if (flag) {
+            $.ajax(contentPath + "/chgPass",{
+                data: $('#chgPassForm').serialize(),
+                type: "POST",
+                success: function (result) {
+                    if (result === "success") {
+                        new PNotify({
+                            title: 'Success',
+                            text: '密码修改成功！',
+                            type: 'success',
+                            styling: 'bootstrap3'
+                        });
+                    } else {
+
+                    }
+                }
+            });
+        } else {
+            return false;
+        }*/
+    })
+
 });
