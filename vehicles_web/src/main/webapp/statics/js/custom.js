b1885fdbf5421571ebb8b53d1f8ce6f45df40a53
daniel <lendoon@gmail.com>
2017-06-06 13:35:36 +0800

    modify custom js

--- vehicles_web/src/main/webapp/statics/js/custom.js
+++ vehicles_web/src/main/webapp/statics/js/custom.js
@@ -1838,33 +1838,11 @@ if (typeof NProgress != 'undefined') {
 	  	/* PNotify */
 			
 		function init_PNotify() {
-			
+
 			if( typeof (PNotify) === 'undefined'){ return; }
 			console.log('init_PNotify');
-			
-			new PNotify({
-			  title: "PNotify",
-			  type: "info",
-			  text: "Welcome. Try hovering over me. You can click things behind me, because I'm non-blocking.",
-			  nonblock: {
-				  nonblock: true
-			  },
-			  addclass: 'dark',
-			  styling: 'bootstrap3',
-			  hide: false,
-			  before_close: function(PNotify) {
-				PNotify.update({
-				  title: PNotify.options.title + " - Enjoy your Stay",
-				  before_close: null
-				});
 
-				PNotify.queueRemove();
-
-				return false;
-			  }
-			});
-
-		}; 
+		};
 	   
 	   
 	   /* CUSTOM NOTIFICATION */
